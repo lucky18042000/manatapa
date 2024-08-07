97008e495b23d2d8dc5ff3f39318555d5758e315
lucky182000 <rohitsingh182000@gmail.com>
2024-08-07 21:58:47 +0530

    Latest Added Firebase working

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import localFont from 'next/font/local';
 import Header from '@/Components/Header';
+import { collection, getDocs, getFirestore } from 'firebase/firestore';
+import { db } from './firebaseConfig';
 
 const roslindaleFont = localFont({
     src: '../fonts/Roslindale-DisplayNarrowRegular-Testing.ttf',
@@ -9,13 +11,44 @@ const roslindaleFont = localFont({
     variable: '--font-roslindale'
 });
 function HomePage() {
+    const [videoLinks, setVideoLinks] = useState([]);
+    const [zoomoutImages, setZoomImages] = useState([])
+    const db = getFirestore(); // Assuming you've initialized Firebase properly
+    const homepageVideoCollectionRef = collection(db, 'homepageVideo');
+    const homepageZoomOutImagesCollectionRef = collection(db, 'homepageZoomOutImages');
+
+    const fetchVideoData = async () => {
+        try {
+            const querySnapshot = await getDocs(homepageVideoCollectionRef);
+            const links = querySnapshot.docs.map(doc => doc.data());
+            setVideoLinks(links[0]);
+        } catch (error) {
+            console.error('Error fetching data: ', error);
+        }
+    };
+    const fetchZoomOutImages = async () => {
+        try {
+            const querySnapshot = await getDocs(homepageZoomOutImagesCollectionRef);
+            const links = querySnapshot.docs.map(doc => doc.data());
+            setZoomImages(links[0]);
+        } catch (error) {
+            console.error('Error fetching data: ', error);
+        }
+    };
+    useEffect(() => {
+        fetchVideoData();
+        fetchZoomOutImages();
+    }, []);
+    console.log(zoomoutImages);
+
     return (
         <div className=''>
             <Header />
             <div className="homepagesection1">
                 <video
                     className='homepagesection1video'
-                    src="https://firebasestorage.googleapis.com/v0/b/mantapa-22cfd.appspot.com/o/Teaser.mp4?alt=media&token=97687974-3321-46b1-b1dc-4de878bf0f3f"
+                    poster={videoLinks.thumbnail}
+                    src={videoLinks.video}
                     autoPlay
                     loop
                     muted
@@ -27,14 +60,14 @@ function HomePage() {
             <div className='homepagesection2'>
                 <div className='text-center'>
                     <p className={`homepagesection2text  ${roslindaleFont.className}`}>Preserving
-                        <img className='homepagesection2img' src="https://s3-alpha-sig.figma.com/img/aad8/d46d/ea9ed141e5092da75c7ec0b31504bda3?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=qzBMGHzs9XKNN9oS51emmd3sU7DuCtZyA6PIjD3yQfdl3DfeVNMPuNscbAl-J3RKOusUP-zBLTYWo-AYhlGXR3lZPfU6p3zfuSDXl27TSRpURslzqnen7foSTEycTubIXnnRLi-7wjCAbOIt-WfKGUmUyqZZZDM~UDG9oU91YDIpS7c6Z-LSX673q9MWD1nxi4bqSCeb64socH41KR6eFxlzq5e2BK6bTfMMYZuPPiMKv90v~FXR69M~aD8PbbYBfa-TumkjvB2dlZjtelz8PiUTbSkrzbxahAcZ3Zc00U75CbVqkf8Gk2IfaxJUwHpe9ky~VEPfn2n3B6oHcRCsUQ__" alt="" />
+                        <img className='homepagesection2img' src={zoomoutImages?.image} alt="" />
                         your moments into </p>
                     <p className={`homepagesection2text  ${roslindaleFont.className}`}>timeless memories
-                        <img className='homepagesection2img' src="https://s3-alpha-sig.figma.com/img/aad8/d46d/ea9ed141e5092da75c7ec0b31504bda3?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=qzBMGHzs9XKNN9oS51emmd3sU7DuCtZyA6PIjD3yQfdl3DfeVNMPuNscbAl-J3RKOusUP-zBLTYWo-AYhlGXR3lZPfU6p3zfuSDXl27TSRpURslzqnen7foSTEycTubIXnnRLi-7wjCAbOIt-WfKGUmUyqZZZDM~UDG9oU91YDIpS7c6Z-LSX673q9MWD1nxi4bqSCeb64socH41KR6eFxlzq5e2BK6bTfMMYZuPPiMKv90v~FXR69M~aD8PbbYBfa-TumkjvB2dlZjtelz8PiUTbSkrzbxahAcZ3Zc00U75CbVqkf8Gk2IfaxJUwHpe9ky~VEPfn2n3B6oHcRCsUQ__" alt="" />
+                        <img className='homepagesection2img' src={zoomoutImages?.image2} alt="" />
                         through </p>
                     <br />
                     <p className={`homepagesection2text  ${roslindaleFont.className}`}>
-                        <img className='homepagesection2img' src="https://s3-alpha-sig.figma.com/img/aad8/d46d/ea9ed141e5092da75c7ec0b31504bda3?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=qzBMGHzs9XKNN9oS51emmd3sU7DuCtZyA6PIjD3yQfdl3DfeVNMPuNscbAl-J3RKOusUP-zBLTYWo-AYhlGXR3lZPfU6p3zfuSDXl27TSRpURslzqnen7foSTEycTubIXnnRLi-7wjCAbOIt-WfKGUmUyqZZZDM~UDG9oU91YDIpS7c6Z-LSX673q9MWD1nxi4bqSCeb64socH41KR6eFxlzq5e2BK6bTfMMYZuPPiMKv90v~FXR69M~aD8PbbYBfa-TumkjvB2dlZjtelz8PiUTbSkrzbxahAcZ3Zc00U75CbVqkf8Gk2IfaxJUwHpe9ky~VEPfn2n3B6oHcRCsUQ__" alt="" />
+                        <img className='homepagesection2img !w-[118px] 1h-[90px]' src={zoomoutImages?.image3} alt="" />
                         our lens... </p>
                     <div className='w-full flex flex-col justify-center items-center'>
                         <p className='w-[60%] text-[#A80018] capitalize text-[20px] lg:mt-[72px]'>Lorem ipsum dolor sit amet consectetur. Non mi quis sit tellus amet nunc sem. Blandit auctor eu ridiculus pharetra vel non consequat. Nunc a ut dui accumsan nunc at urna. Libero id aliquam duis egestas aliquet vitae.eu ridiculus pharetra vel non consequat. Nunc a ut dui accumsan nunc at urna. Libero id aliquam duis egestas aliquet vitae.</p>
---
+++ src/pages/firebaseConfig.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD0v45DoYw5msqwV41Gd3BUCN2RyJphREg",
+    authDomain: "mantapa-22cfd.firebaseapp.com",
+    projectId: "mantapa-22cfd",
+    storageBucket: "mantapa-22cfd.appspot.com",
+    messagingSenderId: "1073909669316",
+    appId: "1:1073909669316:web:814264ef4ce817489c5891",
+    measurementId: "G-3SKYDZ9YEC"
+};
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export default { db };
