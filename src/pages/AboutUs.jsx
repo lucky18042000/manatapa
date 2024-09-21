54e16e117b56d1960420e83f47605214991e3dc3
lucky182000 <rohitsingh182000@gmail.com>
2024-09-21 16:00:04 +0530

    latest

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,7 +1,10 @@
 import Footer from '@/Components/Footer'
 import Header from '@/Components/Header'
-import React from 'react'
+import React, { useRef } from 'react'
 import localFont from 'next/font/local';
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger'; // Ensure ScrollTrigger is imported from 'gsap/dist/ScrollTrigger'
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
 
 const roslindaleFont = localFont({
     src: '../fonts/Roslindale-DisplayNarrowRegular-Testing.ttf',
@@ -9,7 +12,52 @@ const roslindaleFont = localFont({
     style: 'normal',
     variable: '--font-roslindale'
 });
+
+gsap.registerPlugin(ScrollTrigger);
+
 function AboutUs() {
+    const main = useRef(); // Reference for the container element
+
+
+    useGSAP(() => {
+        const leftImages = gsap.utils.toArray('.left-image');
+        leftImages.forEach((leftImage) => {
+            gsap.fromTo(
+                leftImage,
+                { x: '50%', opacity: 0 },
+                {
+                    x: '-100px',
+                    opacity: 1,
+                    duration: 1.5,
+                    scrollTrigger: {
+                        trigger: leftImage,
+                        start: 'top 50%',
+                        end: 'bottom 50%',
+                        scrub: true,
+                    },
+                }
+            );
+        });
+
+        const rightImages = gsap.utils.toArray('.right-image');
+        rightImages.forEach((rightImage) => {
+            gsap.fromTo(
+                rightImage,
+                { x: '-50%', opacity: 0 },
+                {
+                    x: '100px',
+                    opacity: 1,
+                    duration: 1.5,
+                    scrollTrigger: {
+                        trigger: rightImage,
+                        start: 'top 50%',
+                        end: 'bottom 50%',
+                        scrub: true,
+                    },
+                }
+            );
+        });
+    }, []); // Empty array ensures this runs only once
     return (
         <div>
             <Header />
@@ -23,7 +71,7 @@ function AboutUs() {
             <div className='aboutussection2'>
                 <img src="" alt="" srcset="" />
             </div>
-            <div className='aboutussection3'>
+            {/* <div className='aboutussection3'>
                 <div className='absolute lg:left-[-100px] left-[-100px] h-full flex items-center'>
                     <img className='lg:w-[600px] lg:h-[700px] w-[216px] h-[347px] object-cover rotate-[-2.8deg] rounded-[32px] -z-10' src='https://firebasestorage.googleapis.com/v0/b/mantapa-22cfd.appspot.com/o/647e3cc83822b06137a15c00_Header20Left-p-1080.jpg.png?alt=media&token=6ab2cded-a4c7-4a21-9602-e33866957612' alt="" />
                 </div>
@@ -34,7 +82,26 @@ function AboutUs() {
                 <div className='absolute lg:right-[-100px] right-[-115px] h-full flex items-center'>
                     <img className='lg:w-[600px] lg:h-[700px] w-[216px] h-[347px]  object-cover rotate-[2.8deg] rounded-[32px] -z-10' src='https://firebasestorage.googleapis.com/v0/b/mantapa-22cfd.appspot.com/o/647e3cc83822b06137a15c00_Header20Left-p-1080.jpg.png?alt=media&token=6ab2cded-a4c7-4a21-9602-e33866957612' alt="" />
                 </div>
-            </div>
+            </div> */}
+            <div className='aboutussection3'>
+                    <div className='absolute lg:left-[-100px] left-[-100px] h-full flex items-center'>
+                        <img
+                            className="left-image lg:w-[600px] lg:h-[700px] w-[216px] h-[347px] object-cover rotate-[-2.8deg] rounded-[32px] -z-10"
+                            src='https://firebasestorage.googleapis.com/v0/b/mantapa-22cfd.appspot.com/o/647e3cc83822b06137a15c00_Header20Left-p-1080.jpg.png?alt=media&token=6ab2cded-a4c7-4a21-9602-e33866957612'
+                            alt="" />
+                    </div>
+                    <div className=' flex flex-col justify-center items-center w-[413px]' >
+                    <h1 className={`lg:text-[84.9px] text-[26px]  text-[#A80018] lg:leading-[98px] leading-[24px] font-bold text-center ${roslindaleFont.className}`}>Where we Work</h1>
+                    <p className='lg:text-[18px] text-[14px]  text-[#A80018] leading-[19px] lg:leading-[29px] lg:mt-[61px] w-[254px] lg:w-full text-center capitalize'>From the bustling streets of New York to the historic charm of London, we are across the USA and UK, soon to embrace the vibrant and diverse landscapes of India.</p>
+                </div>
+                    <div className="absolute lg:right-[-100px] right-[-115px] h-full flex items-center">
+                        <img
+                            className="right-image lg:w-[600px] lg:h-[700px] w-[216px] h-[347px] object-cover rotate-[2.8deg] rounded-[32px] -z-10"
+                            src="https://firebasestorage.googleapis.com/v0/b/mantapa-22cfd.appspot.com/o/647e3cc83822b06137a15c00_Header20Left-p-1080.jpg.png?alt=media&token=6ab2cded-a4c7-4a21-9602-e33866957612"
+                            alt="Right Image"
+                        />
+                    </div>
+                </div>
             <div className='aboutussection4'>
                 <h1 className={`text-[36px] lg:text-[84.9px] text-[#A80018] leading-[98px] font-bold ${roslindaleFont.className}`}>Nature of Mantapa</h1>
                 <div className='flex lg:flex-row flex-col gap-[65px] mt-[54px]'>
